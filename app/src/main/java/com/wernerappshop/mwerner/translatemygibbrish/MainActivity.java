33bd1d8a99ee2591632240d7aaffbd5fec3837af
Michael Werner <michaelw3@gmail.com>
2015-07-24 12:39:02 +0300

    first stage shared intent

--- app/src/main/java/com/wernerappshop/mwerner/translatemygibbrish/MainActivity.java
+++ app/src/main/java/com/wernerappshop/mwerner/translatemygibbrish/MainActivity.java
@@ -1,23 +1,56 @@
 package com.wernerappshop.mwerner.translatemygibbrish;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import de.greenrobot.event.EventBus;
+
 public class MainActivity extends ActionBarActivity {
     private Button translateButton = null;
     private EditText textToDecode = null;
+    private String tempText = null;
     private TextView decodedText = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        //findig edittext and insert shared text
+        textToDecode = (EditText) findViewById(R.id.textToDecode);
+        translateButton = (Button) findViewById(R.id.button);
+        decodedText = (TextView) findViewById(R.id.decodedText);
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            }
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
     }
 
+    @Override
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+    // Called in Android UI's main thread
+    public void onEventMainThread(DecodeReadyEvent event) {
+        decodedText.setText(event.getDecodedString());
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -25,8 +58,7 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+        public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -39,4 +71,12 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            // Update UI to reflect text being shared
+            textToDecode.setText(sharedText, TextView.BufferType.EDITABLE);
+        }
+    }
+
 }
